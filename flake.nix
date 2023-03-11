0abd32271c6d371cfe8fcb95667c016ac39d7c84
Nicole Wren <wrenn@squareup.com>
2023-03-11 16:59:37 -0500

    Add emmet-ls to flake inputs

--- flake.nix
+++ flake.nix
@@ -2,19 +2,18 @@
   description = "My Neovim configurations";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
-    };
+    # TODO(wrenn): https://github.com/NixOS/nixpkgs/pull/178737
+    nixpkgs-emmet-ls.url = "github:NixOS/nixpkgs?rev=177e8d2647a8eba1e03f4d50ee73597671145180";
   };
 
-  outputs = { self, nixpkgs, flake-utils, flake-compat }:
+  outputs = { self, nixpkgs, flake-utils, nixpkgs-emmet-ls }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         inherit (nixpkgs) lib;
         pkgs = nixpkgs.legacyPackages.${system};
+
         neovim =
           let
             nvim = pkgs.neovim.override {
@@ -32,6 +31,9 @@
               wrapProgram $out/bin/nvim --prefix PATH : ${lib.makeBinPath deps}
             '';
           });
+
+        emmet-ls = nixpkgs-emmet-ls.legacyPackages.${system}.nodePackages.emmet-ls;
+
         deps = with pkgs; [
           # core
           xxd
@@ -52,6 +54,7 @@
           sumneko-lua-language-server
           gopls
           terraform-ls
+          emmet-ls
           nodePackages.vscode-langservers-extracted
           python310Packages.python-lsp-server
           python310Packages.python-lsp-black
