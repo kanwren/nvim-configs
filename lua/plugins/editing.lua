d966f5608318f9096ef4f77443eef293919cc918
Nicole Wren <wrenn@squareup.com>
2023-04-18 17:08:52 -0400

    Remove gJ and gS for splitjoin

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -18,8 +18,6 @@ return {
   {
     'Wansmer/treesj',
     keys = {
-      { 'gS', '<cmd>TSJSplit<CR>',  mode = { 'n' }, noremap = true, desc = 'split node' },
-      { 'gJ', '<cmd>TSJJoin<CR>',   mode = { 'n' }, noremap = true, desc = 'join node' },
       { 'gT', '<cmd>TSJToggle<CR>', mode = { 'n' }, noremap = true, desc = 'toggle node split' },
     },
     config = {
