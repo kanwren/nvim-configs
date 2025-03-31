b484c90ff406225673cfd16f62cdf88200a9a6e4
Nicole Wren <nicole@wren.systems>
2025-03-30 17:45:56 -0700

    Consolidate plugin config, fix IBL and leap settings

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -13,10 +13,13 @@ return {
     'ggandor/leap.nvim',
     config = function()
       local leap = require('leap')
-      local leap_user = require('leap.user')
+      leap.opts.safe_labels = {}
+
       vim.keymap.set('n', 's', '<Plug>(leap)')
       vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
       vim.keymap.set({ 'x', 'o' }, 's', '<Plug>(leap)')
+
+      vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'Comment' })
     end
   },
 
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -17,6 +17,17 @@ local function hatch(key, creature, speed, description)
   }
 end
 
+local colorizer_filetypes = {
+  'css',
+  'javascript',
+  'javascriptreact',
+  'typescript',
+  'typescriptreact',
+  'html',
+  'vim',
+  'lua',
+}
+
 return {
   -- nicer vim.ui.input (e.g. renaming) and vim.ui.select (e.g. code actions)
   {
@@ -41,6 +52,45 @@ return {
         timeout = 3000,
         stages = 'fade',
       })
+    end,
+  },
+
+  -- LSP status indicator
+  {
+    'j-hui/fidget.nvim',
+    tag = 'v1.2.0',
+    config = true,
+  },
+
+  -- indent guides
+  {
+    'lukas-reineke/indent-blankline.nvim',
+    main = 'ibl',
+    config = function()
+      local ibl = require('ibl')
+      ibl.setup({
+        enabled = false,
+        scope = {
+          enabled = true,
+        },
+        indent = {
+          char = '│',
+        },
+      })
+
+      vim.keymap.set('n', '<Leader>t<Tab>', '<cmd>IBLToggle<CR>', {
+        noremap = true,
+        desc = 'Toggle indent guides',
+      })
+    end,
+  },
+
+  -- show hex codes as colors: #b4befe, #94e2d5
+  {
+    'norcalli/nvim-colorizer.lua',
+    ft = colorizer_filetypes,
+    config = function()
+      require('colorizer').setup(colorizer_filetypes)
     end,
   },
 
